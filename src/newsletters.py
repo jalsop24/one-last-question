e3eb535eea1379985dcd2cb7c3cc3f26f9e7dcaa
James <j.alsop99@hotmail.com>
2024-06-15 15:02:59 +0100

    Added create newsletter

--- src/newsletters.py
+++ src/newsletters.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, Response, g, url_for, redirect
 
 from .auth import login_required
+from .db import get_db
+from .models import Newsletter
+
 
 bp = Blueprint("newsletters", __name__, url_prefix="/newsletters")
 
@@ -8,10 +11,32 @@ bp = Blueprint("newsletters", __name__, url_prefix="/newsletters")
 @bp.route("/")
 @login_required
 def index():
-    return render_template("newsletter.j2")
+    user = g.user
+
+    newsletters = user.newsletters
+
+    return render_template("newsletter.j2", newsletters=newsletters)
 
 
-@bp.route("/create")
+@bp.route("/create", methods=["GET", "POST"])
 @login_required
 def create():
-    return render_template("create_newsletter.j2")
+    if request.method == "GET":
+        return render_template("create_newsletter.j2")
+    elif request.method == "POST":
+
+        print(request.form)
+
+        name = request.form["name"]
+
+        db = get_db()
+
+        newsletter = Newsletter(name=name)
+        newsletter.users = [g.user]
+
+        db.add(newsletter)
+        db.commit()
+
+        return redirect(url_for("newsletters.index"))
+    else:
+        return Response(status=405)
