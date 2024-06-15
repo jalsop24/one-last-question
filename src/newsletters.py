452e92d6dba68c6ce24a94e2ff7b26e1776dd971
James <j.alsop99@hotmail.com>
2024-06-15 15:29:26 +0100

    Added delete button

--- src/newsletters.py
+++ src/newsletters.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, Response, g, url_for, redirect
+import sqlalchemy as sa
 
 from .auth import login_required
 from .db import get_db
@@ -40,3 +41,22 @@ def create():
         return redirect(url_for("newsletters.index"))
     else:
         return Response(status=405)
+
+
+@bp.route("/<int:id>/delete", methods=["DELETE"])
+@login_required
+def delete(id):
+
+    db = get_db()
+
+    newsletter = db.execute(
+        sa.select(Newsletter).where(Newsletter.id == id)
+    ).scalar_one_or_none()
+
+    if newsletter is None:
+        return Response(status=404)
+
+    db.delete(newsletter)
+    db.commit()
+
+    return Response(status=200)
