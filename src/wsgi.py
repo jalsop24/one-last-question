b33635ed324253435f25844906162e8853c4683e
James <j.alsop99@hotmail.com>
2024-06-15 16:30:44 +0100

    Added flask mail

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,6 +1,7 @@
+import json
 import os
 
-from flask import Flask, config
+from flask import Flask
 
 
 def create_app(test_config=None):
@@ -15,12 +16,14 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        app.config.from_pyfile("config.py", silent=True)
+        app.config.from_file("config.json", load=json.load)
     else:
         app.config.from_mapping(test_config)
 
     # Register views / blueprints
-    from . import auth, home, newsletters
+    from . import auth, email, home, newsletters
+
+    email.mail.init_app(app)
 
     app.register_blueprint(home.bp)
     app.register_blueprint(auth.bp)
